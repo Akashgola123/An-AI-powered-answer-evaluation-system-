4ac47b302909f39020f6cefedce87166ff07ac7f
Akashgola123 <golaakash0@gmail.com>
2025-02-19 11:00:18 +0530

    Initial commit

--- setup.py
+++ setup.py
@@ -1,30 +1,32 @@
 import setuptools
 
-with open("README.md", "r",encoding="utf-8") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-
 __version__ = "0.0.0"
-
-REPO_NAME = "An-AI-powered-answer-evaluation-system-"
+REPO_NAME = "An_ai_powered_answer_evaluation_system"
 AUTHOR_USER_NAME = "Akashgola123"
-SRC_REPO = "AI_Assisted_Assessment_System"
+SRC_REPO = "Ai_assisted_assessment_system"
 AUTHOR_EMAIL = "golaakash0@gmail.com"
 
-
 setuptools.setup(
-    name=f"{REPO_NAME}-{SRC_REPO}",
+    name=SRC_REPO,
     version=__version__,
-    author=f"{AUTHOR_USER_NAME}",
-    author_email=f"{AUTHOR_EMAIL}",
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
     description="An AI-powered answer evaluation system using RAG and LLMs to assess student responses by retrieving knowledge from PDFs and question banks.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url = f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
     package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src")
-
+    packages=setuptools.find_packages(where="src"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
 )
